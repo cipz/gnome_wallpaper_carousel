f681c13c4ad15c2ed77bc97912ecfa3f1ab5bd7a
cip <ciprianv96@hotmail.it>
2020-09-10 11:28:09 +0300

    First working version

--- wallpaper_changer.sh
+++ wallpaper_changer.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-config=false
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 config_file=$DIR"/config.json"
 
@@ -31,13 +29,13 @@ parse_args () {
 				shift
 				directory="$1"
 			;;
-			-i|--include)
-				shift
-				include="$1"
-			;;
-			-e|--exclude)
-				shift
-				include="$1"
+#			-i|--include)
+#				shift
+#				include="$1"
+#			;;
+#			-e|--exclude)
+#				shift
+#				include="$1"
 			;;
 			*) break
 		esac
@@ -68,64 +66,64 @@ update_config () {
 
 }
 
+update_wallpaper () {
+
+  # This code allows the script to run with cron
+  # gsettings needs to know the process and the display
+  user=$(whoami)
+
+  fl=$(find /proc -maxdepth 2 -user $user -name environ -print -quit)
+  while [ -z $(grep -z DBUS_SESSION_BUS_ADDRESS "$fl" | cut -d= -f2- | tr -d '\000' ) ]
+  do
+    fl=$(find /proc -maxdepth 2 -user $user -name environ -newer "$fl" -print -quit)
+  done
+
+  export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS "$fl" | cut -d= -f2-)
+
+  # - -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - -- 
+
+  # Getting variables from configuration file
+  directory=$(jq '.directory' ${config_file})
+  directory=${directory//\"}
+
+  old_wallpaper=$(jq '.current_wallpaper' ${config_file})
+  old_wallpaper=${old_wallpaper//\"}
+
+  # Use nullglob in case there are no matching files
+  shopt -s nullglob
+
+  arr=(${directory}/*)
+  size=${#arr[@]}
+
+  # echo $size
+  index=$(($RANDOM % $size))
+  new_wallpaper=${arr[$index]}
+
+  if (( size > 1 ))
+  then
+      while [ $new_wallpaper == $old_wallpaper ]
+      do
+          index=$(($RANDOM % $size))
+          new_wallpaper=${arr[$index]}
+      done
+  fi
+
+  jq --arg a "${new_wallpaper}" '.current_wallpaper = $a' ${config_file} > "tmp" && mv "tmp" ${config_file}
+
+  # echo "Changing wallpaper to $new_wallpaper"
+
+  gsettings set org.gnome.desktop.background picture-uri "${new_wallpaper}"
+
+}
+
 if test $# -gt 0
 then
 
 	parse_args $@
   update_config
 
-#else
-#	echo "No arguments have been passed."
+else
+
+  update_wallpaper
+
 fi
-
-# - -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - -- 
-
-# This code allows the script to run with cron
-# gsettings needs to know the process and the display
-user=$(whoami)
-
-fl=$(find /proc -maxdepth 2 -user $user -name environ -print -quit)
-while [ -z $(grep -z DBUS_SESSION_BUS_ADDRESS "$fl" | cut -d= -f2- | tr -d '\000' ) ]
-do
-  fl=$(find /proc -maxdepth 2 -user $user -name environ -newer "$fl" -print -quit)
-done
-
-export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS "$fl" | cut -d= -f2-)
-
-# - -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - -- - -- 
-
-# Getting variables from configuration file
-directory=$(jq '.directory' ${config_file})
-directory=${directory//\"}
-
-old_wallpaper=$(jq '.current_wallpaper' ${config_file})
-old_wallpaper=${old_wallpaper//\"}
-
-# use nullglob in case there are no matching files
-shopt -s nullglob
-
-arr=(${directory}/*)
-size=${#arr[@]}
-
-# echo $size
-index=$(($RANDOM % $size))
-new_wallpaper=${arr[$index]}
-
-if (( size > 1 ))
-then
-    while [ $new_wallpaper == $old_wallpaper ]
-    do
-        index=$(($RANDOM % $size))
-        new_wallpaper=${arr[$index]}
-    done
-fi
-
-# Or I could take it out of the array
-#arr=("${arr[@]/$old_wallpaper}")
-#let size--
-
-jq --arg a "${new_wallpaper}" '.current_wallpaper = $a' ${config_file} > "tmp" && mv "tmp" ${config_file}
-
-#echo "Changing wallpaper to $new_wallpaper"
-
-gsettings set org.gnome.desktop.background picture-uri "${new_wallpaper}"
